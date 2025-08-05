50833983c87c36646c382159e78815152dd3b570
the7threlic <the7threlic@gmail.com>
2025-08-05 10:20:42 -0400

    Add .next/static/chunks/src_app_page_tsx_650e26a1._.js 1754403642937

---
+++ .next/static/chunks/src_app_page_tsx_650e26a1._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_9dcdf2dc._.js",
+  "static/chunks/src_8c543a6b._.js"
+],
+    source: "dynamic"
+});
