da24c72225ddbaae0a8cb40e97397bd21cb465d3
the7threlic <the7threlic@gmail.com>
2025-08-05 10:20:25 -0400

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1754403625816

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_fda8109e._.js",
+  "static/chunks/src_components_theme-provider_tsx_2dd132ec._.js"
+],
+    source: "dynamic"
+});
